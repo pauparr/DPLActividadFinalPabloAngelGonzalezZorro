00af88174d5e5f55e1fa689a55eef74d0e456cc1
pauparr <pautorres55xx@gmail.com>
2026-05-13 17:01:53 +0100

    Codigo mal 1

--- src/test/java/org/example/dplfinalpabloangelgonzalezzorro/DplfinalpabloangelgonzalezzorroApplicationTests.java
+++ src/test/java/org/example/dplfinalpabloangelgonzalezzorro/DplfinalpabloangelgonzalezzorroApplicationTests.java
@@ -1,9 +1,10 @@
 package org.example.dplfinalpabloangelgonzalezzorro;
 
+import com.tuapp.TuappAplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TuappAplication.class)
 class DplfinalpabloangelgonzalezzorroApplicationTests {
 
     @Test
